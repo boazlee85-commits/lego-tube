55c9f74c3be9e314d93ec4c4cac68822b4f1b019
boazlee85-commits <boazlee85@gmail.com>
2026-02-09 14:26:20 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,55 +1,32 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const multer = require("multer");
-const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
-app.use(cors());
-app.use(express.json());
 app.use(express.static("public"));
 
-/* STORAGE */
-let users = {};
-let channels = {};
-let liveChats = [];
+let broadcaster;
 
-/* FILE UPLOAD */
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "public/uploads"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
-});
-const upload = multer({ storage });
-
-app.post("/upload", upload.fields([{name:"video"},{name:"thumb"}]), (req,res)=>{
-  const { user, title } = req.body;
-  if(!channels[user]) channels[user]={name:user,subs:0,videos:[]};
-  channels[user].videos.push({
-    name:title,
-    video:"/uploads/"+req.files.video[0].filename,
-    thumb:"/uploads/"+req.files.thumb[0].filename,
-    views:0,
-    likes:0,
-    comments:[]
-  });
-  res.json({status:"ok"});
-});
-
-app.get("/channels", (req,res)=>res.json(channels));
-
-/* SOCKET LIVE SYSTEM */
-io.on("connection", socket=>{
-  socket.on("liveChat", msg=>{
-    liveChats.push(msg);
-    io.emit("liveChat", msg);
+io.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
   });
 
-  socket.on("viewerJoin", ()=>{
-    io.emit("viewerCount");
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+
+  socket.on("chat", msg => {
+    io.emit("chat", msg);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("disconnectPeer", socket.id);
   });
 });
 
-server.listen(3000, ()=>console.log("Server running on http://localhost:3000"));
+server.listen(3000, () => console.log("Server running"));
